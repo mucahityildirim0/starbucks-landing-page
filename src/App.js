a98d42a9ff1d1735de3f6c7c68cda664655851ae
Mücahit Yıldırım <mucahityildirim@Mucahit-MacBook-Air.local>
2022-07-10 11:20:55 +0300

    add boxes and styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Boxes from "./components/Boxes";
+import Footer from "./components/Footer";
 import "./App.css";
 
 const App = () => {
   return (
     <div>
       <Navbar />
+      <Boxes />
+      <Footer />
     </div>
   );
 };
